e8c65e32574fece0bfc300d6cda8675ba50a04aa
Sam Reilly <REIL2762@pacificmags.com.au>
2020-03-04 10:37:19 +1100

    Space Age complete

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -4,12 +4,36 @@
 //
 //Use a case function
 export const age = (planet, ageInSeconds) => {
-  let calcAge;
+  let calcAge=0;
+
+  let earthAge = ageInSeconds / 31557600;
 
   switch (planet) {
-    case 'Venus':
-      age = ageInSeconds * 1;
+    case 'earth':
+      calcAge = earthAge;
+      break;
+    case 'mercury':
+      calcAge=earthAge / 0.2408467;
+      break;
+        case 'venus':
+      calcAge=earthAge / 0.61519726;
+      break;
+        case 'mars':
+      calcAge=earthAge / 1.8808158;
+      break;
+        case 'jupiter':
+      calcAge=earthAge / 11.862615;
+      break;
+        case 'saturn':
+      calcAge=earthAge / 29.447498;
+      break;
+        case 'uranus':
+      calcAge=earthAge / 84.016846;
+      break;
+        case 'neptune':
+      calcAge=earthAge / 164.79132;
       break;
   }
+  return (parseFloat(calcAge.toFixed(2)));
 
 };
