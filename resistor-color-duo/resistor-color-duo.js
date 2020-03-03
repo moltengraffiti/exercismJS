5662f60dd0e02957c7ca356c0c122372327ba5f2
Sam Reilly <REIL2762@pacificmags.com.au>
2020-03-03 14:11:54 +1100

    Reistor color duo passed

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const COLOURS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+let value;
+
+export const decodedValue = ([colour1, colour2]) => {
+  if (COLOURS.includes(colour1) && COLOURS.includes(colour2)) {
+    value = COLOURS.indexOf(colour1)*10+COLOURS.indexOf(colour2);;
+    return value;
+
+  }
+}
