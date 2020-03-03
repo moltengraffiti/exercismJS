62c9b8d232b7852b354476276d9f97230323eee3
Sam Reilly <REIL2762@pacificmags.com.au>
2020-03-03 17:00:44 +1100

    start space age build

---
+++ space-age/space-age.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+//Use a case function
+export const age = (planet, ageInSeconds) => {
+  let calcAge;
+
+  switch (planet) {
+    case 'Venus':
+      age = ageInSeconds * 1;
+      break;
+  }
+
+};
